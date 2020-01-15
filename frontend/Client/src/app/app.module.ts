c16a0fcd571f8d3457debb0af894d13ca67198d3
Stefan <s3ri0us22@gmail.com>
2020-01-15 18:34:33 +0100

    Frontend
    -Added empty component classes for home page

--- frontend/Client/src/app/app.module.ts
+++ frontend/Client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GroupComponent } from './group/group.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GroupComponent,
   ],
   imports: [
     BrowserModule
