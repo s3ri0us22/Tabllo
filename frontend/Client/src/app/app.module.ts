3f8bde4b19cb34af1f9b10cd8245d37cef89ed61
Stefan <s3ri0us22@gmail.com>
2020-01-15 19:04:10 +0100

    Frontend
    - fix mistake

--- frontend/Client/src/app/app.module.ts
+++ frontend/Client/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { GroupComponent } from './group/group.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { GroupComponent } from "./group/group.component";
+import { GroupCardComponent } from "./group/group-card/group-card.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     GroupComponent,
+    GroupCardComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
